49b516ee8d05f0764ad53a3e8dac50a882222e54
Daniel Nelson <codemrnelson@gmail.com>
2023-05-06 20:39:30 -0400

    Make navigation menu button interactive

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,8 @@
 "use strict";
+
+// Navigation Menu JS
+const navMenuBtn = document.querySelector('.btn-menu');
+
+navMenuBtn.addEventListener('click', function() {
+    document.querySelector('.nav-links').classList.toggle('hidden');
+})
