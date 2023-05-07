d00be8a057a33e3c37bc41238ab0ea49f1aafa4a
Daniel Nelson <codemrnelson@gmail.com>
2023-05-07 12:35:43 -0400

    Create getComputerChoice function

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
 "use strict";
-
+////////////
 // Navigation Menu JS
+////////////
 const navMenuBtn = document.querySelector('.btn-menu');
 
 navMenuBtn.addEventListener('click', function() {
     document.querySelector('.nav-links').classList.toggle('hidden');
 })
+
+////////////
+// Rock Paper Scissors Game
+////////////
+function getComputerChoice() {
+    const cpuChoices = ['rock', 'paper', 'scissors'];
+    return cpuChoices[Math.floor(Math.random() * cpuChoices.length)];
+}
+
+console.log(getComputerChoice());
