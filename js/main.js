8a5e9eef269586640a954e484222ab866f034be9
Daniel Nelson <codemrnelson@gmail.com>
2023-05-07 13:13:39 -0400

    Create functions to reset scores and display them

--- js/main.js
+++ js/main.js
@@ -31,4 +31,49 @@ function game() {
     }
 }
 
-game();
+// game();
+
+////////////
+// Rock Paper Scissors Game
+////////////
+
+// DOM Variables
+const gameStartBtn = document.querySelector('.btn-game');
+const roundNumberEl = document.querySelector('.round-number');
+const playerScoreEl = document.querySelector('.player-score');
+const cpuScoreEl = document.querySelector('.cpu-score');
+
+// Game Variables
+let roundNumber = 0;
+let playerScore = 0;
+let cpuScore = 0;
+
+// Display Game Text
+function displayGameHeader() {
+    const gameHeader = document.querySelector('.game-header');
+    const gameText = document.querySelector('.game-text');
+    gameHeader.classList.remove('hidden');
+    gameText.classList.remove('hidden');
+}
+
+// Reset Game Round, Score, & Choices
+function resetGameScores() {
+    roundNumber = 0;
+    playerScore = 0;
+    cpuScore = 0;
+}
+
+function displayGameScores() {
+    roundNumberEl.textContent = roundNumber;
+    playerScoreEl.textContent = playerScore;
+    cpuScoreEl.textContent = cpuScore;
+}
+
+
+gameStartBtn.addEventListener('click', startGame);
+
+function startGame() {    
+    displayGameHeader();
+    resetGameScores();
+    displayGameScores();
+}
