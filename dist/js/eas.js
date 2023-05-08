3caab1f46b126db4e5dda1a79e5fdf4ac2ce5e68
Daniel Nelson <codemrnelson@gmail.com>
2023-05-08 12:53:40 -0400

    Create a single cell only using JavaScript and update the grid

--- dist/js/eas.js
+++ dist/js/eas.js
@@ -1,1 +1,37 @@
 "use strict";
+
+////////////
+// Etch-a-Sketch - Global Variables
+////////////
+
+// DOM Variables
+const easContainer = document.querySelector('.container-eas');
+const colorInput = document.getElementById('eas-color');
+const rangeInput = document.getElementById('eas-range');
+
+// Global Variables
+let range = 1;
+
+////////////
+// Etch-a-Sketch - Functions
+////////////
+function createSquares(range) {
+    for(let i = 0; i < range; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('eas-square');
+        easContainer.appendChild(cell);
+    }
+    updateGrid(range);
+}
+
+function updateGrid(range) {
+    easContainer.style.gridTemplateColumns = `repeat(${range}, 1fr)`;
+    easContainer.style.gridTemplateRows = `repeat(${range}, 1fr)`;
+}
+
+createSquares(range);
+
+rangeInput.addEventListener('change', () => {
+    range = rangeInput.value;
+    console.log(range);
+})
