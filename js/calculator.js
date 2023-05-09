e7b5dfd0626927f835578689e358b5a66d0ed188
Daniel Nelson <codemrnelson@gmail.com>
2023-05-09 13:43:00 -0400

    Create basic arithmetic functions

--- js/calculator.js
+++ js/calculator.js
@@ -1,1 +1,32 @@
 "use strict";
+
+////////////
+// Etch-a-Sketch - Global Variables
+////////////
+
+// DOM Variables
+const calcInputDisplay = document.querySelector('.p-calc-display')
+const calcInputResult = document.querySelector('.p-calc');
+const calcBtns = document.querySelectorAll('.btn-calc');
+
+// Calculator Functions
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+console.log(add(1, 2));
+console.log(subtract(3, 2));
+console.log(multiply(3, 2));
+console.log(divide(9, 3));
