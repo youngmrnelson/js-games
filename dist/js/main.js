67cc64a4feae105c05ed8507aa3de712fe88c75b
Daniel Nelson <codemrnelson@gmail.com>
2023-05-15 12:17:01 -0400

    Set up ESLint/Prettier/Airbnb style

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,8 +1,8 @@
-////////////
+/// /////////
 // Navigation Menu JS
-////////////
+/// /////////
 const navMenuBtn = document.querySelector('.btn-menu');
 
-navMenuBtn.addEventListener('click', function() {
-    document.querySelector('.nav-links').classList.toggle('hidden');
-})
+navMenuBtn.addEventListener('click', () => {
+  document.querySelector('.nav-links').classList.toggle('hidden');
+});
