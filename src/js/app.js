1aae14ffeeeef29dc42207adcd8ef4acc2d5ed0d
Daniel Nelson <codemrnelson@gmail.com>
2023-05-20 16:31:59 -0400

    Create loading effect when a game is selected

--- src/js/app.js
+++ src/js/app.js
@@ -5,14 +5,42 @@ class App {
   navMenuBtn = document.querySelector('.btn-menu');
   navLinksEl = document.querySelector('.nav-links');
 
+  // Loading SVG
+  loadingSVG = document.getElementById('loading'); 
+
+  // Game Buttons
+  gameBtns = document.querySelectorAll('.game-btn');
+
   constructor() {
     // Event Listeners
     this.navMenuBtn.addEventListener('click', this.#toggleMenu.bind(this));
+    this.gameBtns.forEach(btn => btn.addEventListener('click', this.#startGame.bind(this)));
   }
 
   // Navigation Menu Functions
   #toggleMenu() {
     this.navLinksEl.classList.toggle('hidden');
+  }
+
+  #test() {
+    console.log('Hello');
+  }
+
+  #startGame() {
+    while (this.#game.firstChild) {
+        this.#game.removeChild(this.#game.firstChild);
+    }
+    this.#loading();
+  }
+
+  #loading() {
+    this.loadingSVG.classList.remove('hidden');
+    this.loadingSVG.classList.add('display-block');
+    this.#game.appendChild(this.loadingSVG);
+    setTimeout(() => {
+        this.loadingSVG.classList.add('hidden');
+        this.loadingSVG.classList.remove('display-block');
+    }, 1500);
   }
 }
 
