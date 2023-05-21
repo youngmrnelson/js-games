9953bbcb5c7292eb8c1d76fbc633dc41f0bbd09a
Daniel Nelson <codemrnelson@gmail.com>
2023-05-21 13:31:07 -0400

    Dynamically create each game board

--- src/js/app.js
+++ src/js/app.js
@@ -16,7 +16,13 @@ class App {
     this.gameBtns.forEach(btn => btn.addEventListener('click', this.#startGame.bind(this)));
     this.gameBtns.forEach(btn => btn.addEventListener('click', () => {
       if(btn.textContent === 'Rock Paper Scissors') {
-        this.#createBoard();
+        this.#createBoard('rps');
+      }
+      if(btn.textContent === 'Etch a Sketch') {
+        this.#createBoard('eas');
+      }
+      if(btn.textContent === 'Calculator') {
+        this.#createBoard('calc');
       }
     }))
   }
@@ -43,65 +49,125 @@ class App {
     }, 1500);
   }
 
-  #createBoard() {
-    const html = ` <h2 class="h h-game mb-2">ROCK PAPER SCISSORS</h2>
-      <div class="container-rps ta-c p-3 grid grid-gc--2">
-        <button class="btn btn-game p-2 grid-col--full ps-c">Start Game</button>
-        <p class="h h-game grid-col--full hidden game-header">
-          Round #<span class="round-number">0</span>
-        </p>
-        <p class="p-game grid-col--full hidden game-text">Pick your weapon</p>
-        <div class="p-scores player-scoreboard ps-c">
-          <p class="p-game">Score: <span class="player-score">0</span></p>
-          <p class="p-game">
-            <i class="fa-solid fa-user i-game-player" aria-hidden="true"></i>
+  #createBoard(game) {
+    if(game === 'rps') {
+          const html = `<h2 class="h h-game mb-2">ROCK PAPER SCISSORS</h2>
+        <div class="container-rps ta-c p-3 grid grid-gc--2">
+          <button class="btn btn-game p-2 grid-col--full ps-c">Start Game</button>
+          <p class="h h-game grid-col--full hidden game-header">
+            Round #<span class="round-number">0</span>
           </p>
-          <p class="p-game">
-            User
-            <span class="player-choice">?</span>
-          </p>
-        </div>
-        <div class="p-scores cpu-scoreboard ps-c">
-          <p class="p-game">Score: <span class="cpu-score">0</span></p>
-          <p class="p-game">
-            <i
-              class="fa-solid fa-user-astronaut i-game-player"
-              aria-hidden="true"
-            ></i>
-          </p>
-          <p class="p-game">
-            <span class="cpu-choice">?</span>
-            CPU
-          </p>
-        </div>
-        <p class="p-game game-message grid-col--full"></p>
+          <p class="p-game grid-col--full hidden game-text">Pick your weapon</p>
+          <div class="p-scores player-scoreboard ps-c">
+            <p class="p-game">Score: <span class="player-score">0</span></p>
+            <p class="p-game">
+              <i class="fa-solid fa-user i-game-player" aria-hidden="true"></i>
+            </p>
+            <p class="p-game">
+              User
+              <span class="player-choice">?</span>
+            </p>
+          </div>
+          <div class="p-scores cpu-scoreboard ps-c">
+            <p class="p-game">Score: <span class="cpu-score">0</span></p>
+            <p class="p-game">
+              <i
+                class="fa-solid fa-user-astronaut i-game-player"
+                aria-hidden="true"
+              ></i>
+            </p>
+            <p class="p-game">
+              <span class="cpu-choice">?</span>
+              CPU
+            </p>
+          </div>
+          <p class="p-game game-message grid-col--full"></p>
+          <div
+            class="m-auto rps-choices grid-col--full grid grid-gc--2 gap-3 my-3 hidden"
+          >
+            <button class="btn btn-rps gap-3 btn-rps-rock ps-c">
+              <i
+                class="fa-regular fa-hand-back-fist btn-rps-choice-icon mb-1"
+                aria-hidden="true"
+              ></i>
+              <span class="h btn-rps-choice disp-block">ROCK</span>
+            </button>
+            <button class="btn btn-rps btn-rps-paper ps-c">
+              <i
+                class="fa-regular fa-hand btn-rps-choice-icon mb-1"
+                aria-hidden="true"
+              ></i>
+              <span class="h btn-rps-choice disp-block">PAPER</span>
+            </button>
+            <button class="btn btn-rps btn-rps-scissors grid-col--full ps-c">
+              <i
+                class="fa-regular fa-hand-scissors btn-rps-choice-icon mb-1"
+                aria-hidden="true"
+              ></i>
+              <span class="h btn-rps-choice disp-block">SCISSORS</span>
+            </button>
+          </div>
+        </div>`;
+      this.#game.insertAdjacentHTML('afterbegin', html);
+      this.#game.className = '';
+      this.#game.classList.add('my-4', 'flex', 'flex-fd--c', 'ai-c', 'jc-c', 'p-2', 'full-screen');
+    }
+    if(game === 'eas') {
+      const html = `
+        <h2 class="h h-game mb-2">ETCH-A-SKETCH</h2>
         <div
-          class="m-auto rps-choices grid-col--full grid grid-gc--2 gap-3 my-3 hidden"
+        class="eas-controls-container mb-2 flex flex-fd--c-mobile gap-2 ai-c"
         >
-          <button class="btn btn-rps gap-3 btn-rps-rock ps-c">
-            <i
-              class="fa-regular fa-hand-back-fist btn-rps-choice-icon mb-1"
-              aria-hidden="true"
-            ></i>
-            <span class="h btn-rps-choice disp-block">ROCK</span>
-          </button>
-          <button class="btn btn-rps btn-rps-paper ps-c">
-            <i
-              class="fa-regular fa-hand btn-rps-choice-icon mb-1"
-              aria-hidden="true"
-            ></i>
-            <span class="h btn-rps-choice disp-block">PAPER</span>
-          </button>
-          <button class="btn btn-rps btn-rps-scissors grid-col--full ps-c">
-            <i
-              class="fa-regular fa-hand-scissors btn-rps-choice-icon mb-1"
-              aria-hidden="true"
-            ></i>
-            <span class="h btn-rps-choice disp-block">SCISSORS</span>
-          </button>
+        <input type="color" id="eas-color" />
+        <button class="btn btn-eas btn-eas-color p-2">Color Mode</button>
+        <button class="btn btn-eas btn-eas-rainbow p-2">Rainbow Mode</button>
+        <button class="btn btn-eas btn-eas-eraser p-2">Eraser</button>
+        <button class="btn btn-eas btn-eas-clear p-2">Clear</button>
+        <input type="range" id="eas-range" min="1" max="32" /><span
+          id="eas-range-num"
+        ></span>
+      </div>
+      <div class="container-eas eas-grid"></div>
+      `;
+      this.#game.insertAdjacentHTML('afterbegin', html);
+      this.#game.className = '';
+      this.#game.classList.add('my-4', 'flex', 'flex-fd--c', 'ai-c', 'jc-c', 'p-2');
+    }
+    if(game === 'calc') {
+      const html = `
+        <h2 class="h h-game mb-2">Calculator</h2>
+        <div class="container container-calc grid grid-gc--4 gap-1 p-3">
+        <!-- Calculator -->
+        <div class="calc-display grid-col--full p-3">
+          <p class="p-calc-display ta-r">0</p>
+          <p class="p-calc ta-r">0</p>
         </div>
-      </div>`;
-    this.#game.insertAdjacentHTML('afterbegin', html);
+        <!-- Top Row -->
+        <button class="btn btn-calc btn-calc-alt p-3 grid-s--3">AC</button>
+        <button class="btn btn-calc btn-op p-3">/</button>
+        <!-- Calc Row -->
+        <button class="btn btn-calc btn-calc-num p-3">7</button>
+        <button class="btn btn-calc btn-calc-num p-3">8</button>
+        <button class="btn btn-calc btn-calc-num p-3">9</button>
+        <button class="btn btn-calc btn-op p-3">*</button>
+        <!-- Calc Row -->
+        <button class="btn btn-calc btn-calc-num p-3">4</button>
+        <button class="btn btn-calc btn-calc-num p-3">5</button>
+        <button class="btn btn-calc btn-calc-num p-3">6</button>
+        <button class="btn btn-calc btn-op p-3">-</button>
+        <!-- Calc Row -->
+        <button class="btn btn-calc btn-calc-num p-3">1</button>
+        <button class="btn btn-calc btn-calc-num p-3">3</button>
+        <button class="btn btn-calc btn-calc-num p-3">3</button>
+        <button class="btn btn-calc btn-op p-3">+</button>
+        <!-- Calc Row -->
+        <button class="btn btn-calc btn-calc-num p-3 grid-s--3">0</button>
+        <button class="btn btn-calc btn-op p-3">=</button>
+      </div>
+      `;
+      this.#game.insertAdjacentHTML('afterbegin', html);
+      this.#game.className = '';
+      this.#game.classList.add('my-4', 'flex', 'flex-fd--c', 'ai-c', 'jc-c', 'p-2');    }
   }
 }
 
--- src/js/rps.js
+++ src/js/rps.js
@@ -1,225 +1,225 @@
-class RockPaperScissors {
-  #game = document.getElementById('game');
-  // DOM Variables
-  gameStartBtn = document.querySelector('.btn-game');
-  roundNumberEl = document.querySelector('.round-number');
-  playerScoreEl = document.querySelector('.player-score');
-  playerChoiceEl = document.querySelector('.player-choice');
-  cpuScoreEl = document.querySelector('.cpu-score');
-  cpuChoiceEl = document.querySelector('.cpu-choice');
-  gameMessage = document.querySelector('.game-message');
-  rpsChoicesContainer = document.querySelector('.rps-choices');
-  rockBtn = document.querySelector('.btn-rps-rock');
-  paperBtn = document.querySelector('.btn-rps-paper');
-  scissorsBtn = document.querySelector('.btn-rps-scissors');
+// class RockPaperScissors {
+//   #game = document.getElementById('game');
+//   // DOM Variables
+//   gameStartBtn = document.querySelector('.btn-game');
+//   roundNumberEl = document.querySelector('.round-number');
+//   playerScoreEl = document.querySelector('.player-score');
+//   playerChoiceEl = document.querySelector('.player-choice');
+//   cpuScoreEl = document.querySelector('.cpu-score');
+//   cpuChoiceEl = document.querySelector('.cpu-choice');
+//   gameMessage = document.querySelector('.game-message');
+//   rpsChoicesContainer = document.querySelector('.rps-choices');
+//   rockBtn = document.querySelector('.btn-rps-rock');
+//   paperBtn = document.querySelector('.btn-rps-paper');
+//   scissorsBtn = document.querySelector('.btn-rps-scissors');
 
-  // Game Variables
-  #roundNumber = 0;
-  #playerScore = 0;
-  #cpuScore = 0;
-  #winningScore = 5;
+//   // Game Variables
+//   #roundNumber = 0;
+//   #playerScore = 0;
+//   #cpuScore = 0;
+//   #winningScore = 5;
 
-  constructor() {
-    // Event Listeners
-    this.#createBoard();
-    // this.gameStartBtn.addEventListener('click', this.#startGame.bind(this));
-    // this.rockBtn.addEventListener('click', function() { this.#playGame('rock').bind(this)});
-    // this.paperBtn.addEventListener('click', function() { this.#playGame('paper').bind(this)});
-    // this.scissorsBtn.addEventListener('click', function() { this.#playGame('scissors').bind(this)});
-  }
+//   constructor() {
+//     // Event Listeners
+//     this.#createBoard();
+//     this.gameStartBtn.addEventListener('click', this.#startGame.bind(this));
+//     this.rockBtn.addEventListener('click', function() { this.#playGame('rock').bind(this)});
+//     this.paperBtn.addEventListener('click', function() { this.#playGame('paper').bind(this)});
+//     this.scissorsBtn.addEventListener('click', function() { this.#playGame('scissors').bind(this)});
+//   }
 
-  #getComputerChoice() {
-    const cpuChoices = ['rock', 'paper', 'scissors'];
-    return cpuChoices[Math.floor(Math.random() * cpuChoices.length)]
-  }
+//   #getComputerChoice() {
+//     const cpuChoices = ['rock', 'paper', 'scissors'];
+//     return cpuChoices[Math.floor(Math.random() * cpuChoices.length)]
+//   }
 
-  #displayGameHeader() {
-    const gameHeader = document.querySelector('.game-header');
-    const gameText = document.querySelector('.game-text');
-    gameHeader.classList.toggle('hidden');
-    gameText.classList.toggle('hidden');
-  }
+//   #displayGameHeader() {
+//     const gameHeader = document.querySelector('.game-header');
+//     const gameText = document.querySelector('.game-text');
+//     gameHeader.classList.toggle('hidden');
+//     gameText.classList.toggle('hidden');
+//   }
 
-  #resetGameScore() {
-    this.#roundNumber = 0;
-    this.#playerScore = 0;
-    this.#cpuScore = 0;
-    this.playerChoiceEl.innerHTML = '?';
-    this.cpuChoiceEl.innerHTML = '?';
-  }
+//   #resetGameScore() {
+//     this.#roundNumber = 0;
+//     this.#playerScore = 0;
+//     this.#cpuScore = 0;
+//     this.playerChoiceEl.innerHTML = '?';
+//     this.cpuChoiceEl.innerHTML = '?';
+//   }
 
-  #displayGameScores() {
-    this.roundNumberEl.textContent = this.#roundNumber;
-    this.playerScoreEl.textContent = this.#playerScore;
-    this.cpuScoreEl.textContent = this.#cpuScore;
-  }
+//   #displayGameScores() {
+//     this.roundNumberEl.textContent = this.#roundNumber;
+//     this.playerScoreEl.textContent = this.#playerScore;
+//     this.cpuScoreEl.textContent = this.#cpuScore;
+//   }
 
-  #displayGameButtons() {
-    this.rpsChoicesContainer.classList.toggle('hidden');
-  }
+//   #displayGameButtons() {
+//     this.rpsChoicesContainer.classList.toggle('hidden');
+//   }
 
-  #startGame() {
-    this.gameStartBtn.style.display = 'none';
-    this.gameMessage.textContent = '';
-    this.#displayGameHeader();
-    this.#nobodyWinsGame();
-    this.#resetGameScore();
-    this.#displayGameScores();
-    this.#displayGameButtons();
-  }
+//   #startGame() {
+//     this.gameStartBtn.style.display = 'none';
+//     this.gameMessage.textContent = '';
+//     this.#displayGameHeader();
+//     this.#nobodyWinsGame();
+//     this.#resetGameScore();
+//     this.#displayGameScores();
+//     this.#displayGameButtons();
+//   }
 
-  #endGame() {
-    this.gameStartBtn.style.display = 'block';
-    this.#displayGameHeader();
-    this.displayGameButtons();
-  }
+//   #endGame() {
+//     this.gameStartBtn.style.display = 'block';
+//     this.#displayGameHeader();
+//     this.displayGameButtons();
+//   }
 
-  #updateGameRound() {
-    this.#roundNumber++;
-    this.roundNumberEl.textContent = this.#roundNumber;
-  }
+//   #updateGameRound() {
+//     this.#roundNumber++;
+//     this.roundNumberEl.textContent = this.#roundNumber;
+//   }
 
-  #playerWinsRound() {
-    this.#playerScore++;
-    this.playerScoreEl.textContent = this.#playerScore;
-    this.gameMessage.textContent = 'You won!';
-  }
+//   #playerWinsRound() {
+//     this.#playerScore++;
+//     this.playerScoreEl.textContent = this.#playerScore;
+//     this.gameMessage.textContent = 'You won!';
+//   }
 
-  #cpuWinsRound() {
-    this.#cpuScore++;
-    this.cpuScoreEl.textContent = this.#cpuScore;
-    this.gameMessage.textContent = 'You lost...';
-  }
+//   #cpuWinsRound() {
+//     this.#cpuScore++;
+//     this.cpuScoreEl.textContent = this.#cpuScore;
+//     this.gameMessage.textContent = 'You lost...';
+//   }
 
-  #updatePlayerChoices(playerSelection, computerSelection) {
-    this.playerChoiceEl.textContent = playerSelection.toUpperCase();
-    this.cpuChoiceEl.textContent = computerSelection.toUpperCase();
-  }
+//   #updatePlayerChoices(playerSelection, computerSelection) {
+//     this.playerChoiceEl.textContent = playerSelection.toUpperCase();
+//     this.cpuChoiceEl.textContent = computerSelection.toUpperCase();
+//   }
 
-  #playerWinsGame() {
-    this.playerScoreEl.classList.add('p-win');
-    this.playerChoiceEl.classList.add('p-win');
-    this.cpuScoreEl.classList.add('p-lose');
-    this.cpuChoiceEl.classList.add('p-lose');
-    this.#endGame();
-  }
+//   #playerWinsGame() {
+//     this.playerScoreEl.classList.add('p-win');
+//     this.playerChoiceEl.classList.add('p-win');
+//     this.cpuScoreEl.classList.add('p-lose');
+//     this.cpuChoiceEl.classList.add('p-lose');
+//     this.#endGame();
+//   }
 
-  #cpuWinsGame() {
-    this.playerScoreEl.classList.add('p-lose');
-    this.playerChoiceEl.classList.add('p-lose');
-    this.cpuScoreEl.classList.add('p-win');
-    this.cpuChoiceEl.classList.add('p-win');
-    this.#endGame();
-  }
+//   #cpuWinsGame() {
+//     this.playerScoreEl.classList.add('p-lose');
+//     this.playerChoiceEl.classList.add('p-lose');
+//     this.cpuScoreEl.classList.add('p-win');
+//     this.cpuChoiceEl.classList.add('p-win');
+//     this.#endGame();
+//   }
 
-  #nobodyWinsGame() {
-    this.playerScoreEl.classList.remove('p-win');
-    this.playerChoiceEl.classList.remove('p-win');
-    this.cpuScoreEl.classList.remove('p-win');
-    this.cpuChoiceEl.classList.remove('p-win');
-    this.playerScoreEl.classList.remove('p-lose');
-    this.playerChoiceEl.classList.remove('p-lose');
-    this.cpuScoreEl.classList.remove('p-lose');
-    this.cpuChoiceEl.classList.remove('p-lose');
-  }
+//   #nobodyWinsGame() {
+//     this.playerScoreEl.classList.remove('p-win');
+//     this.playerChoiceEl.classList.remove('p-win');
+//     this.cpuScoreEl.classList.remove('p-win');
+//     this.cpuChoiceEl.classList.remove('p-win');
+//     this.playerScoreEl.classList.remove('p-lose');
+//     this.playerChoiceEl.classList.remove('p-lose');
+//     this.cpuScoreEl.classList.remove('p-lose');
+//     this.cpuChoiceEl.classList.remove('p-lose');
+//   }
 
-  #checkForWinner() {
-    if (this.#playerScore !== this.#winningScore && this.#cpuScore !== this.#winningScore) {
-      return;
-    }
-    if (this.#playerScore === this.#winningScore) {
-      this.#playerWinsGame();
-    } else if (this.#cpuScore === this.#winningScore) {
-      this.#cpuWinsGame();
-    }
-  }
+//   #checkForWinner() {
+//     if (this.#playerScore !== this.#winningScore && this.#cpuScore !== this.#winningScore) {
+//       return;
+//     }
+//     if (this.#playerScore === this.#winningScore) {
+//       this.#playerWinsGame();
+//     } else if (this.#cpuScore === this.#winningScore) {
+//       this.#cpuWinsGame();
+//     }
+//   }
 
-  #playRound(playerSelection) {
-    const computerSelection = this.#getComputerChoice();
-    this.#updatePlayerChoices(playerSelection, computerSelection);
+//   #playRound(playerSelection) {
+//     const computerSelection = this.#getComputerChoice();
+//     this.#updatePlayerChoices(playerSelection, computerSelection);
 
-    if (playerSelection === computerSelection) {
-      this.#updateGameRound();
-      gameMessage.textContent = "It's a draw.";
-    } else if (
-      (playerSelection === 'rock' && computerSelection === 'scissors') ||
-      (playerSelection === 'paper' && computerSelection === 'rock') ||
-      (playerSelection === 'scissors' && computerSelection === 'paper')
-    ) {
-      this.#updateGameRound();
-      this.#playerWinsRound();
-      this.#checkForWinner();
-    } else {
-      this.#updateGameRound();
-      this.#cpuWinsRound();
-      this.#checkForWinner();
-    }
-  }
+//     if (playerSelection === computerSelection) {
+//       this.#updateGameRound();
+//       gameMessage.textContent = "It's a draw.";
+//     } else if (
+//       (playerSelection === 'rock' && computerSelection === 'scissors') ||
+//       (playerSelection === 'paper' && computerSelection === 'rock') ||
+//       (playerSelection === 'scissors' && computerSelection === 'paper')
+//     ) {
+//       this.#updateGameRound();
+//       this.#playerWinsRound();
+//       this.#checkForWinner();
+//     } else {
+//       this.#updateGameRound();
+//       this.#cpuWinsRound();
+//       this.#checkForWinner();
+//     }
+//   }
 
-  #playGame(playerSelection) {
-    this.#playRound(playerSelection);
-  }
+//   #playGame(playerSelection) {
+//     this.#playRound(playerSelection);
+//   }
 
-  #createBoard() {
-    const html = ` <h2 class="h h-game mb-2">ROCK PAPER SCISSORS</h2>
-      <div class="container-rps ta-c p-3 grid grid-gc--2">
-        <button class="btn btn-game p-2 grid-col--full ps-c">Start Game</button>
-        <p class="h h-game grid-col--full hidden game-header">
-          Round #<span class="round-number">0</span>
-        </p>
-        <p class="p-game grid-col--full hidden game-text">Pick your weapon</p>
-        <div class="p-scores player-scoreboard ps-c">
-          <p class="p-game">Score: <span class="player-score">0</span></p>
-          <p class="p-game">
-            <i class="fa-solid fa-user i-game-player" aria-hidden="true"></i>
-          </p>
-          <p class="p-game">
-            User
-            <span class="player-choice">?</span>
-          </p>
-        </div>
-        <div class="p-scores cpu-scoreboard ps-c">
-          <p class="p-game">Score: <span class="cpu-score">0</span></p>
-          <p class="p-game">
-            <i
-              class="fa-solid fa-user-astronaut i-game-player"
-              aria-hidden="true"
-            ></i>
-          </p>
-          <p class="p-game">
-            <span class="cpu-choice">?</span>
-            CPU
-          </p>
-        </div>
-        <p class="p-game game-message grid-col--full"></p>
-        <div
-          class="m-auto rps-choices grid-col--full grid grid-gc--2 gap-3 my-3 hidden"
-        >
-          <button class="btn btn-rps gap-3 btn-rps-rock ps-c">
-            <i
-              class="fa-regular fa-hand-back-fist btn-rps-choice-icon mb-1"
-              aria-hidden="true"
-            ></i>
-            <span class="h btn-rps-choice disp-block">ROCK</span>
-          </button>
-          <button class="btn btn-rps btn-rps-paper ps-c">
-            <i
-              class="fa-regular fa-hand btn-rps-choice-icon mb-1"
-              aria-hidden="true"
-            ></i>
-            <span class="h btn-rps-choice disp-block">PAPER</span>
-          </button>
-          <button class="btn btn-rps btn-rps-scissors grid-col--full ps-c">
-            <i
-              class="fa-regular fa-hand-scissors btn-rps-choice-icon mb-1"
-              aria-hidden="true"
-            ></i>
-            <span class="h btn-rps-choice disp-block">SCISSORS</span>
-          </button>
-        </div>
-      </div>`;
-    this.#game.insertAdjacentHTML('afterbegin', html);
-  }
-}
+//   #createBoard() {
+//     const html = ` <h2 class="h h-game mb-2">ROCK PAPER SCISSORS</h2>
+//       <div class="container-rps ta-c p-3 grid grid-gc--2">
+//         <button class="btn btn-game p-2 grid-col--full ps-c">Start Game</button>
+//         <p class="h h-game grid-col--full hidden game-header">
+//           Round #<span class="round-number">0</span>
+//         </p>
+//         <p class="p-game grid-col--full hidden game-text">Pick your weapon</p>
+//         <div class="p-scores player-scoreboard ps-c">
+//           <p class="p-game">Score: <span class="player-score">0</span></p>
+//           <p class="p-game">
+//             <i class="fa-solid fa-user i-game-player" aria-hidden="true"></i>
+//           </p>
+//           <p class="p-game">
+//             User
+//             <span class="player-choice">?</span>
+//           </p>
+//         </div>
+//         <div class="p-scores cpu-scoreboard ps-c">
+//           <p class="p-game">Score: <span class="cpu-score">0</span></p>
+//           <p class="p-game">
+//             <i
+//               class="fa-solid fa-user-astronaut i-game-player"
+//               aria-hidden="true"
+//             ></i>
+//           </p>
+//           <p class="p-game">
+//             <span class="cpu-choice">?</span>
+//             CPU
+//           </p>
+//         </div>
+//         <p class="p-game game-message grid-col--full"></p>
+//         <div
+//           class="m-auto rps-choices grid-col--full grid grid-gc--2 gap-3 my-3 hidden"
+//         >
+//           <button class="btn btn-rps gap-3 btn-rps-rock ps-c">
+//             <i
+//               class="fa-regular fa-hand-back-fist btn-rps-choice-icon mb-1"
+//               aria-hidden="true"
+//             ></i>
+//             <span class="h btn-rps-choice disp-block">ROCK</span>
+//           </button>
+//           <button class="btn btn-rps btn-rps-paper ps-c">
+//             <i
+//               class="fa-regular fa-hand btn-rps-choice-icon mb-1"
+//               aria-hidden="true"
+//             ></i>
+//             <span class="h btn-rps-choice disp-block">PAPER</span>
+//           </button>
+//           <button class="btn btn-rps btn-rps-scissors grid-col--full ps-c">
+//             <i
+//               class="fa-regular fa-hand-scissors btn-rps-choice-icon mb-1"
+//               aria-hidden="true"
+//             ></i>
+//             <span class="h btn-rps-choice disp-block">SCISSORS</span>
+//           </button>
+//         </div>
+//       </div>`;
+//     this.#game.insertAdjacentHTML('afterbegin', html);
+//   }
+// }
 
-export default new RockPaperScissors();
+// export default new RockPaperScissors();
